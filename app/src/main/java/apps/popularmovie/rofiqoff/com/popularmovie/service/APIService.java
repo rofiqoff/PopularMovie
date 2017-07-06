43a194d61e787a1c425a458d43445a256122dae0
rofiqoff <rofiqof@gmail.com>
2017-07-06 16:41:55 +0700

    seventh update - ..

--- app/src/main/java/apps/popularmovie/rofiqoff/com/popularmovie/service/APIService.java
+++ app/src/main/java/apps/popularmovie/rofiqoff/com/popularmovie/service/APIService.java
@@ -1,5 +1,6 @@
 package apps.popularmovie.rofiqoff.com.popularmovie.service;
 
+import apps.popularmovie.rofiqoff.com.popularmovie.BuildConfig;
 import apps.popularmovie.rofiqoff.com.popularmovie.model.GenreModel;
 import apps.popularmovie.rofiqoff.com.popularmovie.model.MovieDetailModel;
 import apps.popularmovie.rofiqoff.com.popularmovie.model.MovieModel;
@@ -17,28 +18,24 @@ import retrofit2.http.Path;
 
 public interface APIService {
 
-    String API_KEY = "0ae4468ac9e27e1c733ab56f3f875064";
     String MOVIE_URL = "http://api.themoviedb.org/3/movie/";
 
-    @GET("popular?api_key=" + API_KEY)
+    @GET("popular?api_key=" + BuildConfig.MOVIE_DB_API_KEY)
     Call<MovieModel> getPopularMovie();
 
-    @GET("top_rated?api_key=" + API_KEY)
+    @GET("top_rated?api_key=" + BuildConfig.MOVIE_DB_API_KEY)
     Call<MovieModel> getTopRatedMovie();
 
-    @GET("genre/movie/list?api_key=" + API_KEY)
+    @GET("genre/movie/list?api_key=" + BuildConfig.MOVIE_DB_API_KEY)
     Call<GenreModel> getGenreList();
 
-    @GET("{movie_id}?api_key=" + API_KEY)
+    @GET("{movie_id}?api_key=" + BuildConfig.MOVIE_DB_API_KEY)
     Call<MovieDetailModel> getDetailMovie(@Path("movie_id") Integer movie_id);
 
-    @GET("{movie_id}?api_key=" + API_KEY)
-    Call<MovieDetailModel.ProductionCountriesData> getCountries(@Path("movie_id") Integer movie_id);
-
-    @GET("{movie_id}/reviews?api_key=" + API_KEY)
+    @GET("{movie_id}/reviews?api_key=" + BuildConfig.MOVIE_DB_API_KEY)
     Call<ReviewModel> getReview(@Path("movie_id") Integer movie_id);
 
-    @GET("{movie_id}/videos?api_key=" + API_KEY)
+    @GET("{movie_id}/videos?api_key=" + BuildConfig.MOVIE_DB_API_KEY)
     Call<TrailerModel> getVideoTrailer(@Path("movie_id") Integer movie_id);
 
     class Factory {
